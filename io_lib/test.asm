cf39dae5e5bd1e773eacceb409185e5e5e6d9269
Denis Smirnov <darthunix@gmail.com>
2018-12-17 23:49:49 +1000

    Implement string_length call and test for it. Remove exit test message.

--- io_lib/test.asm
+++ io_lib/test.asm
@@ -2,13 +2,18 @@
 global _start
 
 section .data
-message: db "Exit test have passed!", 10
+test_string_length: db "failed: string_length", 10, 0   ; 22 + null-terminator
 
 section .text
 _start:
-    mov rax, 1          ; rax contains systemcall for write
-    mov rdi, 1          ; rdi contains file descriptor for stdout (1)
-    mov rsi, message    ; rsi contains an address of string to output
-    mov rdx, 23         ; rdx contains output string length
+    mov rdi, test_string_length ; set rdi string address ('string_length' argument)
+    call string_length
+    cmp rax, 22             ; check counted string length be 22
+    je .end
+    mov rdx, rax            ; set rdx output string length (stored in rax)
+    mov rax, 1              ; set rax systemcall for write
+    mov rdi, 1              ; set rdi file descriptor for stdout (1)
+    mov rsi, test_string_length      ; set rsi an address of string to output
     syscall
-    call exit
+    .end:
+        call exit
