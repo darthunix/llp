708551d264e1e23128a1f6b28cda1e0d95010ae7
Denis Smirnov <darthunix@gmail.com>
2018-12-18 23:28:48 +1000

    Implement call print_string. Changed tests

--- io_lib/test.asm
+++ io_lib/test.asm
@@ -2,18 +2,21 @@
 global _start
 
 section .data
-test_string_length: db "failed: string_length", 10, 0   ; 22 + null-terminator
+success_string_length: db "success: string_length", 10, 0
+fail_string_length: db "failed: string_length", 10, 0   ; 22 + null-terminator
 
 section .text
 _start:
-    mov rdi, test_string_length ; set rdi string address ('string_length' argument)
+    ; test call string_length
+    mov rdi, fail_string_length ; set rdi string address ('string_length' argument)
     call string_length
-    cmp rax, 22             ; check counted string length be 22
-    je .end
-    mov rdx, rax            ; set rdx output string length (stored in rax)
-    mov rax, 1              ; set rax systemcall for write
-    mov rdi, 1              ; set rdi file descriptor for stdout (1)
-    mov rsi, test_string_length      ; set rsi an address of string to output
-    syscall
-    .end:
-        call exit
+    cmp rax, 22                 ; check counted string length be 22
+    je .passed_string_length
+        mov rdi, fail_string_length
+        call print_string
+        jmp .finish_string_length
+    .passed_string_length:
+        mov rdi, success_string_length
+        call print_string
+    .finish_string_length:
+    call exit
