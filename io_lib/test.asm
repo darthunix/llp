6a0614ca9335e9e710436b1a32ef6bd749e2d2a0
Denis Smirnov <darthunix@gmail.com>
2019-01-05 17:52:10 +1000

    Add print_char call

--- io_lib/test.asm
+++ io_lib/test.asm
@@ -4,6 +4,8 @@ global _start
 section .data
 success_string_length: db "success: string_length", 10, 0
 fail_string_length: db "failed: string_length", 10, 0   ; 22 + null-terminator
+message_string_char: db "If you see '$' char test have passed", 10, 0
+test_string_char: db 0x24 
 
 section .text
 _start:
@@ -19,4 +21,9 @@ _start:
         mov rdi, success_string_length
         call print_string
     .finish_string_length:
+    ; test call string_char
+    mov rdi, message_string_char
+    call print_string
+    mov rdi, test_string_char
+    call print_char
     call exit
